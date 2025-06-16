223c1996696ee3e00871942a1da9c51a0548157f
EmirCoban <125053917+EmirCobanOfficial@users.noreply.github.com>
2025-06-16 19:32:55 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@ import Logo from "./logo";
 import SearchForm from "./SearchForm";
 import WatchListButton from "./WatchListButton";
 
-export default function Header() {
+export default function Header({ watchListMovies, onSetIsWatchListOpen }) {
   return (
-    <div id="Header">
+    <div id="header">
       <nav
         className="navbar navbar-expand-lg bg-dark border-bottom border-body"
         data-bs-theme="dark"
@@ -12,7 +12,10 @@ export default function Header() {
         <div className="container">
           <Logo />
           <SearchForm />
-          <WatchListButton />
+          <WatchListButton
+            movies={watchListMovies}
+            onSetIsWatchListOpen={onSetIsWatchListOpen}
+          />
         </div>
       </nav>
     </div>
