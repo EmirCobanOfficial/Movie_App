97f8b8926fd958030d7e4cd788a459709bbada7f
EmirCoban <125053917+EmirCobanOfficial@users.noreply.github.com>
2025-06-18 01:03:36 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,14 @@ import { useState } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
+
+import Logo from "./components/logo";
+import SearchForm from "./components/SearchForm";
+import WatchListButton from "./components/WatchListButton";
+
+import MovieList from "./components/MovieList";
+import WatchList from "./components/WatchList";
+
 import { movie_list } from "./data";
 
 export default function App() {
@@ -9,17 +17,37 @@ export default function App() {
   const [watchListMovies, setWatchListMovies] = useState([]);
   const [isWatchListOpen, setIsWatchListOpen] = useState(false);
 
+  function handleAddToWatchList(movie) {
+    const isAddedToList = watchListMovies.map((i) => i.id).includes(movie.id);
+
+    if (!isAddedToList) {
+      setWatchListMovies((movies) => [...movies, movie]);
+    }
+  }
+
+  function handleRemoveFromWatchList(movie) {
+    setWatchListMovies((movies) => movies.filter((i) => i.id !== movie.id));
+  }
+
   return (
     <>
-      <Header
-        watchListMovies={watchListMovies}
-        onSetIsWatchListOpen={setIsWatchListOpen}
-      />
-      <Main
-        movies={movies}
-        watchListMovies={watchListMovies}
-        isWatchListOpen={isWatchListOpen}
-      />
+      <Header>
+        <Logo />
+        <SearchForm />
+        <WatchListButton
+          movies={watchListMovies}
+          onSetIsWatchListOpen={setIsWatchListOpen}
+        />
+      </Header>
+
+      <Main>
+        <WatchList
+          movies={watchListMovies}
+          isWatchListOpen={isWatchListOpen}
+          onRemoveFromWatchList={handleRemoveFromWatchList}
+        />
+        <MovieList movies={movies} onAddToList={handleAddToWatchList} />
+      </Main>
       <Footer />
     </>
   );
