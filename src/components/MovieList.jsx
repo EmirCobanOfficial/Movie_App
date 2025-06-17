7c456ef823422cba68db3d2093d0a6113ebd3a75
EmirCoban <125053917+EmirCobanOfficial@users.noreply.github.com>
2025-06-18 01:06:43 +0300

    Update MovieList.jsx

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,23 +1,22 @@
 import Movie from "./Movie";
-import { movie_list } from "../data";
 
-export default function MovieList() {
+export default function MovieList({ movies, onAddToList }) {
   return (
-    <div className="container">
+    <div className="container my-3">
       <div className="card">
         <div className="card-header">
           <h2 className="title h5 mb-0">Movie List</h2>
         </div>
         <div className="card-body">
-          {movie_list.filter((m) => m.is_active).length == 0 ? (
+          {movies.filter((m) => m.is_active).length == 0 ? (
             <div>Film bulunamadı</div>
           ) : (
             <div
               id="movie-list"
               className="row row-cols-2 row-cols-md-3 row-cols-lg-4 g-4"
             >
-              {movie_list.map((m, index) => (
-                <Movie key={index} movieObj={m} />
+              {movies.map((m, index) => (
+                <Movie key={index} movieObj={m} onAddToList={onAddToList} />
               ))}
             </div>
           )}
